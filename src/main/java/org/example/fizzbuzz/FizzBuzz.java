af6213f26f561188fb7add64ea042327a9e9e2d1
jpivi <pivijulien@gmail.com>
2024-02-07 06:24:00 +0100

    Step 6 - After refactoring

--- src/main/java/org/example/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/example/fizzbuzz/FizzBuzz.java
@@ -2,7 +2,7 @@ package org.example.fizzbuzz;
 
 public class FizzBuzz {
     public String handle(int value) {
-        if(value == 3 || value == 6 || value == 9) { //Need to refactor now.
+        if (value % 3 == 0) {
             return "fizz";
         }
         return String.valueOf(value);
