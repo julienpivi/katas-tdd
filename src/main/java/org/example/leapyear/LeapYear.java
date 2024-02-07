0aad635b8b3e8f198a70d92f31631bf5724d4c65
jpivi <pivijulien@gmail.com>
2024-02-07 07:37:35 +0100

    Step 7 - add main

--- src/main/java/org/example/leapyear/LeapYear.java
+++ src/main/java/org/example/leapyear/LeapYear.java
@@ -1,5 +1,7 @@
 package org.example.leapyear;
 
+import java.util.Scanner;
+
 public class LeapYear {
     public boolean detect(int value) {
         if(!(value % 4  == 0)) {
@@ -9,5 +11,20 @@ public class LeapYear {
             return false;
         }
         return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LeapYear leapYear = new LeapYear();
+
+        while(true) {
+            int year = scanner.nextInt();
+            if(leapYear.detect(year)) {
+                System.out.println("It's a leap year");
+            } else {
+                System.out.println("It's not a leap year");
+            }
+        }
+
     }
 }
