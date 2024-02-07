5a1bce6ec18d6221e32aed4ba3fb4e4533ee5ec1
jpivi <pivijulien@gmail.com>
2024-02-07 06:12:04 +0100

    Step 3 - After Refactoring

--- src/main/java/org/example/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/example/fizzbuzz/FizzBuzz.java
@@ -2,13 +2,6 @@ package org.example.fizzbuzz;
 
 public class FizzBuzz {
     public String handle(int value) {
-        if(value == 1) {
-            return "1";
-        } else if(value == 2){
-            return "2";
-        } else {
-            return "4";
-        }
-        //We have triple duplication. It's time to refactor it.
+        return String.valueOf(value);
     }
 }
