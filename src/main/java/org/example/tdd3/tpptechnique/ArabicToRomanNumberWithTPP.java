69aaa5afdc0d5841e66eafca1acedfa788a6d136
julien.pivi <julien.pivi@olea-medical.com>
2024-06-06 09:12:15 +0200

    step 3 - refactoring

--- src/main/java/org/example/tdd3/tpptechnique/ArabicToRomanNumberWithTPP.java
+++ src/main/java/org/example/tdd3/tpptechnique/ArabicToRomanNumberWithTPP.java
@@ -3,16 +3,8 @@ package org.example.tdd3.tpptechnique;
 public class ArabicToRomanNumberWithTPP {
 
 	public static String convert(int number) {
-		if(number == 1) {
-			return "I";
-		}
-		if(number == 2) {
-			return "II";
-		}
-		if(number == 3) {
-			return "III";
-		}
-		return null;
+		String[] romanNumber = new String[] {null, "I", "II", "III"};
+		return romanNumber[number];
 	}
 
 }
