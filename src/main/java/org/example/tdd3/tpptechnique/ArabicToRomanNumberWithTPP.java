5108a6d5c45c1d29c25f2352f1ff3c6a610db172
julien.pivi <julien.pivi@olea-medical.com>
2024-06-06 09:17:58 +0200

    step 5 - add ten don't change

--- src/main/java/org/example/tdd3/tpptechnique/ArabicToRomanNumberWithTPP.java
+++ src/main/java/org/example/tdd3/tpptechnique/ArabicToRomanNumberWithTPP.java
@@ -3,7 +3,7 @@ package org.example.tdd3.tpptechnique;
 public class ArabicToRomanNumberWithTPP {
 
 	public static String convert(int number) {
-		String[] romanNumber = new String[] {null, "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+		String[] romanNumber = new String[] {null, "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
 		return romanNumber[number];
 	}
 
--- src/test/java/org/example/tdd3/tpptechnique/ArabicToRomanNumberWithTPPTest.java
+++ src/test/java/org/example/tdd3/tpptechnique/ArabicToRomanNumberWithTPPTest.java
@@ -3,34 +3,34 @@ package org.example.tdd3.tpptechnique;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class ArabicToRomanNumberWithTPPTest {
+class ArabicToRomanNumberWithTPPTest {
 
 	@Test
-	public void zero_then_null() {
+	void zero_then_null() {
 		//assert
 		Assertions.assertEquals(null, ArabicToRomanNumberWithTPP.convert(0));
 	}
 
 	@Test
-	public void one_conversion() {
+	void one_conversion() {
 		//assert
 		Assertions.assertEquals("I", ArabicToRomanNumberWithTPP.convert(1));
 	}
 
 	@Test
-	public void two_conversion() {
+	void two_conversion() {
 		//assert
 		Assertions.assertEquals("II", ArabicToRomanNumberWithTPP.convert(2));
 	}
 
 	@Test
-	public void three_conversion() {
+	void three_conversion() {
 		//assert
 		Assertions.assertEquals("III", ArabicToRomanNumberWithTPP.convert(3));
 	}
 
 	@Test
-	public void four_to_nine_conversion() {
+	void four_to_nine_conversion() {
 		//assert
 		Assertions.assertEquals("IV", ArabicToRomanNumberWithTPP.convert(4));
 		Assertions.assertEquals("V", ArabicToRomanNumberWithTPP.convert(5));
@@ -38,6 +38,12 @@ public class ArabicToRomanNumberWithTPPTest {
 		Assertions.assertEquals("VII", ArabicToRomanNumberWithTPP.convert(7));
 		Assertions.assertEquals("VIII", ArabicToRomanNumberWithTPP.convert(8));
 		Assertions.assertEquals("IX", ArabicToRomanNumberWithTPP.convert(9));
+	}
+
+	@Test
+	void ten_conversion() {
+		//assert
+		Assertions.assertEquals("X", ArabicToRomanNumberWithTPP.convert(10));
 	}
 
 
